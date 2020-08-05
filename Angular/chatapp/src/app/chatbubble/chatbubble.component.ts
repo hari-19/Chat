c092db3522d69df62d7cc6c748fc6df9ce93efbe
hari-19 <hari19@outlook.in>
2020-08-05 15:34:16 +0530

    Updated the chat bubbles & Styles

--- Angular/chatapp/src/app/chatbubble/chatbubble.component.ts
+++ Angular/chatapp/src/app/chatbubble/chatbubble.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chatbubble',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatbubble.component.scss']
 })
 export class ChatbubbleComponent implements OnInit {
+
+  @Input() isSelf:boolean=true;
+  @Input() text:string;
+  @Input() image:string = "../../assets/profile_placeholder.png";
 
   constructor() { }
 
--- Angular/chatapp/src/app/chatroom/chatroom.component.ts
+++ Angular/chatapp/src/app/chatroom/chatroom.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatroomComponent implements OnInit {
 
+  messages = [
+    {
+      text:"Hi",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:true
+    },
+    {
+      text:"How are youasdfassssssssas asdfas df asd fas df asd fas df as dfa sdf as df asd sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      isSelf:false
+    },
+    {
+      text:"I am fine",
+      isSelf:true
+    },
+    {
+      text:"This is a perfect App",
+      isSelf:false
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
