b8254cffc401dd0e3728116e89dd4d4aef22e24e
hari-19 <hari19@outlook.in>
2020-08-05 19:12:22 +0530

    added Scrollbar styles and scroll to bottom

--- Angular/chatapp/src/app/chatroom/chatroom.component.ts
+++ Angular/chatapp/src/app/chatroom/chatroom.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import {faPaperPlane} from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -7,11 +7,112 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   templateUrl: './chatroom.component.html',
   styleUrls: ['./chatroom.component.scss']
 })
-export class ChatroomComponent implements OnInit {
+export class ChatroomComponent implements OnInit, AfterViewInit {
 
   faPaperPlane = faPaperPlane;
+  container:HTMLElement;
 
   messages = [
+    {
+      text:"Hi",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:true
+    },
+    {
+      text:"How are youasdfassssssssas asdfas df asd fas df asd fas df as dfa sdf as df asd sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      isSelf:false
+    },
+    {
+      text:"I am fine",
+      isSelf:true
+    },
+    {
+      text:"This is a perfect App",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:true
+    },
+    {
+      text:"How are youasdfassssssssas asdfas df asd fas df asd fas df as dfa sdf as df asd sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      isSelf:false
+    },
+    {
+      text:"I am fine",
+      isSelf:true
+    },
+    {
+      text:"This is a perfect App",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:true
+    },
+    {
+      text:"How are youasdfassssssssas asdfas df asd fas df asd fas df as dfa sdf as df asd sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      isSelf:false
+    },
+    {
+      text:"I am fine",
+      isSelf:true
+    },
+    {
+      text:"This is a perfect App",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:true
+    },
+    {
+      text:"How are youasdfassssssssas asdfas df asd fas df asd fas df as dfa sdf as df asd sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      isSelf:false
+    },
+    {
+      text:"I am fine",
+      isSelf:true
+    },
+    {
+      text:"This is a perfect App",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:false
+    },
+    {
+      text:"Hi",
+      isSelf:true
+    },
+    {
+      text:"How are youasdfassssssssas asdfas df asd fas df asd fas df as dfa sdf as df asd sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      isSelf:false
+    },
+    {
+      text:"I am fine",
+      isSelf:true
+    },
+    {
+      text:"This is a perfect App",
+      isSelf:false
+    },
     {
       text:"Hi",
       isSelf:false
@@ -38,5 +139,10 @@ export class ChatroomComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {         
+    this.container = document.getElementById("message-box");           
+    this.container.scrollTop = this.container.scrollHeight;     
+  }  
 
 }
